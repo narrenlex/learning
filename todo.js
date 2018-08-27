01cd73bdaa270baa3bd73bca72ea4877b25fed20
Alexey Podgayskiy <narrenlex@gmail.com>
2018-08-27 15:12:23 +0300

    - checkbox & delete button added - css improved

--- todo.js
+++ todo.js
@@ -1,37 +1,52 @@
-function runScript(e) {
-    //See notes about 'which' and 'key'
-    if (e.keyCode == 13) {
+inputTask.addEventListener("keydown", function (event) {
+    if (event.keyCode == 13) {
         addTask();
         return false;
-    }
-}
+    };
+});
 
 function addTask() {
     var task = inputTask.value;
-    //alert(task.value);
     var newTask = document.createElement('p');
     newTask.innerHTML = task;
-    newTask.addEventListener("click", completeTask);
-    newTask.addEventListener("click", activeTaskCount);
     newTask.classList = "task active";
+    var completeButton = document.createElement('input');
+    completeButton.type = 'checkbox';
+    completeButton.classList.add('completeButton');
+    completeButton.onclick = function() {return completeTask(this.parentNode)};
+    newTask.appendChild(completeButton);
+    var deleteButton = document.createElement('button');
+    deleteButton.classList.add('deleteButton', 'invisible');
+    deleteButton.onclick = function() {return deleteTask(this.parentNode)};
+    deleteButton.innerHTML = 'X';
+    newTask.appendChild(deleteButton);
     footer.insertAdjacentElement('beforeBegin', newTask);
     inputTask.value = "";
     taskCount();
     activeTaskCount();
-}
+};
 
-function completeTask() {
-    this.classList.toggle('completed');
-    this.classList.toggle('active');
-}
+function deleteTask(task) {
+    taskList.removeChild(task);
+    taskCount();
+};
+
+function completeTask(task) {
+    task.classList.toggle('completed');
+    task.classList.toggle('active');
+    task.lastChild.classList.toggle('invisible');
+    task.childNodes[1].style.borderColor = 'lightgreen';
+    task.childNodes[1].innerHTML = 'V';
+    activeTaskCount();
+};
 
 function clearCompleted() {
     var elems = document.getElementsByClassName('completed');
     while (elems.length) {
         taskList.removeChild(elems[0]);
-    }
+    };
     taskCount();
-}
+};
 
 function activeTaskCount() {
     var activeTasks = document.getElementsByClassName('active');
@@ -39,9 +54,9 @@ function activeTaskCount() {
         var word = ' task left';
     } else {
         var word = ' tasks left';
-    }
+    };
     counter.innerHTML = activeTasks.length + word;
-}
+};
 
 function taskCount() {
     var tasks = document.getElementsByClassName('task');
@@ -49,10 +64,10 @@ function taskCount() {
         footer.style.display = 'none';
     } else {
         footer.style.display = '';
-    }
-}
+    };
+};
 
-menu.children[0].addEventListener('click', function() {
+menu.children[0].onclick = function() {
     var tasks = document.getElementsByClassName('task');
     for (let i = 0; i < tasks.length; i++) {
         tasks[i].style.display = '';
@@ -60,7 +75,7 @@ menu.children[0].addEventListener('click', function() {
     this.classList.add('menuCurrent');
     menu.children[1].classList.remove('menuCurrent');
     menu.children[2].classList.remove('menuCurrent');
-});
+};
 
 menu.children[1].onclick = function() {
     var activeTasks = document.getElementsByClassName('active');
@@ -74,7 +89,7 @@ menu.children[1].onclick = function() {
     this.classList.add('menuCurrent');
     menu.children[0].classList.remove('menuCurrent');
     menu.children[2].classList.remove('menuCurrent');
-}
+};
 
 menu.children[2].onclick = function() {
     var activeTasks = document.getElementsByClassName('active');
@@ -88,16 +103,9 @@ menu.children[2].onclick = function() {
     this.classList.add('menuCurrent');
     menu.children[0].classList.remove('menuCurrent');
     menu.children[1].classList.remove('menuCurrent');
-}
+};
 
 taskCount();
 activeTaskCount();
-
-inputTask.addEventListener("keydown", function (event) {
-    if (event.keyCode == 13) {
-        addTask();
-        return false;
-    };
-});
 
 elemClearCompleted.addEventListener('click', clearCompleted);
